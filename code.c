9da20b25b480eeadf583a7db2a551994b4943e72
Knowledge has no limit <44311225+jonaxie@users.noreply.github.com>
2019-10-05 15:54:39 +0800

    修改为冒泡排序

--- code.c
+++ code.c
@@ -1,7 +1,27 @@
 ＃include <stdio.h>
 
-
 int main()
 {
-  printf("Hello,world");
+  int i,j,temp;
+  int a[10];
+  
+  for (i = 0; i < 10; i++)
+    scanf("%d", &a[i]);
+  
+  for (i = 0; i < 10; i++)
+  {
+    for (j = 10 - i; j < i; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1]  = temp;
+      }  
+    }
+  }
+  for (i = 0; i < 10; i++)
+    printf("%d", a[i]);
+
+  return 0;
 }
